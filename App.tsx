25be3d9e6d59fe7375f5fe602e5828a23024079e
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-01 23:36:19 -0800

    added navigation, fixed RNSScreenStackHeaderConfig not found error by pinning version react-native-screens: 3.18.0

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
-import { StatusBar, View } from 'react-native';
-// import HomeScreen from './screens/HomeScreen';
-import PlannerScreen from './screens/PlannerScreen';
+import * as React from 'react';
+import { StatusBar } from 'react-native';
+import Navigation from './navigation';
 
 export default function App() {
   return (
-    <View>
-      {/* <HomeScreen /> */}
-      <PlannerScreen />
+    <>
+      <Navigation />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
