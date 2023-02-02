a7efb0688c54936408c57b5f26268edf1610c790
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-01 23:42:51 -0800

    Add buttons on home and planner screens for navigation

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View>
       <Text>I am home screen</Text>
+      <Button
+        title="Go to Planner"
+        onPress={() => alert("Go to planner")}
+      />
     </View>
   );
 }
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,9 +1,13 @@
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export default function PlannerScreen() {
   return (
     <View>
       <Text>I am planner screen</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => alert("Go to Home")}
+      />
     </View>
   );
 }
