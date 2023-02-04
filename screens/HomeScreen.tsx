70e45bd5447fe675da5ac5d5ac638a67402693d9
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-03 20:38:26 -0800

    add native stack header props to home and planner screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { View, Text, Button, Alert } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
+  useEffect(() => {
+    console.log('Rendering Home Screen');
+  }, []);
+
   return (
     <View>
       <Text>I am home screen</Text>
       <Button
         title="Go to Planner"
-        onPress={() => Alert.alert('Go to planner')}
+        onPress={() => navigation.navigate('Planner')}
       />
     </View>
   );
--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import { View, Text, Button, Alert } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, Button } from 'react-native';
+import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 
-export default function PlannerScreen() {
+export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
+  useEffect(() => {
+    console.log('Rendering Planner Screen');
+  }, []);
+
   return (
     <View>
       <Text>I am planner screen</Text>
-      <Button title="Go to Home" onPress={() => Alert.alert('Go to Home')} />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
     </View>
   );
 }
