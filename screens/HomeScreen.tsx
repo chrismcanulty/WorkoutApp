365d5df4eb60a68bed77bbf0763013d84c87a670
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-08 13:26:56 -0800

    render data on home screen, add padding to displayed data

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import data from '../data.json';
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>I am home screen</Text>
+      <Text>{JSON.stringify(data)}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+});
