ceb670d6ec146b11b5c6684233b1f3619acf8605
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-08 16:34:53 -0800

    extract item name and difficulty to filter displayed data

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 import data from '../data.json';
 
 export default function HomeScreen({ navigation }: NativeStackHeaderProps) {
+  // { item }: any -> all props specified as type 'any'; notation below: only {item} has type 'any'
+  const renderItem = ({ item }: { item: any }) => (
+    <View>
+      <Text>{item.name}</Text>
+      <Text>{item.difficulty}</Text>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>I am home screen</Text>
-      <Text>{JSON.stringify(data)}</Text>
+      {/* data must be an array for FlatList */}
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.slug}
+      />
     </View>
   );
 }
