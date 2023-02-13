4e1dbb5bf5366cde58bcf67c2d7063ad96033f05
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-12 17:12:04 -0800

    cleaned up navigation type

--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -3,13 +3,23 @@ import { View, StyleSheet, Text } from 'react-native';
 // import { MontserratText } from '../components/styled/MontserratText';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
 
-export default function WorkoutDetailScreen({ route }: NativeStackHeaderProps) {
+type DetailParams = {
+  route: {
+    params: {
+      slug: string;
+    };
+  };
+};
+
+type Navigation = NativeStackHeaderProps & DetailParams;
+
+export default function WorkoutDetailScreen({ route }: Navigation) {
   // { item }: any -> all props specified as type 'any'; notation below: only {item} has type 'any'
 
   return (
     <View style={styles.container}>
       {/* data must be an array for FlatList */}
-      <Text style={styles.header}>Slug - {(route.params as any).slug}</Text>
+      <Text style={styles.header}>Slug - {route.params.slug}</Text>
       {/* <MontserratText style={{ fontSize: 30 }}>New Workouts</MontserratText> */}
     </View>
   );
