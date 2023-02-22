9ede68646c658dc128e49c016d0ae44aa7e4ede1
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-21 21:58:50 -0800

    set modal visibility and added close function

--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Text, Modal } from 'react-native';
 // import { MontserratText } from '../components/styled/MontserratText';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
@@ -17,7 +17,7 @@ type Navigation = NativeStackHeaderProps & DetailParams;
 
 export default function WorkoutDetailScreen({ route }: Navigation) {
   // { item }: any -> all props specified as type 'any'; notation below: only {item} has type 'any'
-
+  const [isModalVisible, setModalVisible] = useState(false);
   const workout = useWorkoutBySlug(route.params.slug);
 
   return (
@@ -26,11 +26,12 @@ export default function WorkoutDetailScreen({ route }: Navigation) {
       <Text style={styles.header}>{workout?.name}</Text>
       {/* <MontserratText style={{ fontSize: 30 }}>New Workouts</MontserratText> */}
       <PressableText
-        onPress={() => alert('Opening Modal')}
+        onPress={() => setModalVisible(true)}
         text="Check Sequence"
       />
-      <Modal visible={false} transparent={true} animationType="none">
-        <Text>Hello There!</Text>
+      <Modal visible={isModalVisible} transparent={false} animationType="fade">
+        <Text style={{ marginBottom: 100 }}>Hello There!</Text>
+        <PressableText onPress={() => setModalVisible(false)} text="Close" />
       </Modal>
     </View>
   );
