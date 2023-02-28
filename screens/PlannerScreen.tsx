3a8e4f3fc5a57674c5d6d22605c3bc14984122dd
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-27 22:54:16 -0800

    start work on workout form

--- screens/PlannerScreen.tsx
+++ screens/PlannerScreen.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react';
-import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import WorkoutForm from '../components/WorkoutForm';
 
 export default function PlannerScreen({ navigation }: NativeStackHeaderProps) {
-  useEffect(() => {
-    console.log('Rendering Planner Screen');
-  }, []);
-
   return (
-    <View>
-      <Text>I am planner screen</Text>
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+    <View style={styles.container}>
+      <WorkoutForm />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+});
