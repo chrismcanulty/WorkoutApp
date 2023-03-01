5b96d0b2511e8e179dbce0f5083bfe44725fcf02
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-02-28 16:58:18 -0800

    create onChangeText function with state to manage text entered in input field

--- components/WorkoutForm.tsx
+++ components/WorkoutForm.tsx
@@ -1,9 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 
 export default function WorkoutForm() {
+  const [form, setForm] = useState({
+    name: '',
+    duration: '',
+  });
+
+  const onChangeText = (name: string) => (text: string) => {
+    setForm({
+      ...form,
+      [name]: text,
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Text>Exercise Form</Text>
+      <View>
+        <TextInput
+          value={form.name}
+          style={styles.input}
+          onChangeText={onChangeText('name')}
+        />
+        <TextInput
+          value={form.duration}
+          style={styles.input}
+          onChangeText={onChangeText('duration')}
+        />
+      </View>
     </View>
   );
 }
@@ -12,6 +37,12 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     borderRadius: 10,
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
     padding: 10,
   },
 });
