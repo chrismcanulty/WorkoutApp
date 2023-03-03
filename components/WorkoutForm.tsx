2359db1555251ec8e8ab2d9263a8c8fba9e93296
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2023-03-03 14:49:46 -0800

    flex styles for input fields

--- components/WorkoutForm.tsx
+++ components/WorkoutForm.tsx
@@ -21,64 +21,68 @@ export default function WorkoutForm({ onSubmit }: WorkoutProps) {
     <View style={styles.container}>
       <Text>Exercise Form</Text>
       <View>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          name="name"
-          // defaultValue={}
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Name"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          name="duration"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Duration"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="reps"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Repetitions"
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-          }}
-          name="type"
-          render={({ field: { onChange, value } }) => (
-            <TextInput
-              onChangeText={onChange}
-              value={value}
-              style={styles.input}
-              placeholder="Type"
-            />
-          )}
-        />
+        <View style={styles.rowContainer}>
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            name="name"
+            // defaultValue={}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Name"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            name="duration"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Duration"
+              />
+            )}
+          />
+        </View>
+        <View style={styles.rowContainer}>
+          <Controller
+            control={control}
+            name="reps"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Repetitions"
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            rules={{
+              required: true,
+            }}
+            name="type"
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                value={value}
+                style={styles.input}
+                placeholder="Type"
+              />
+            )}
+          />
+        </View>
         <PressableText
           text="Submit"
           onPress={handleSubmit(data => {
@@ -97,11 +101,15 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   input: {
+    flex: 1,
     margin: 2,
     borderWidth: 1,
     height: 30,
     padding: 5,
     borderRadius: 5,
     borderColor: 'rgba(0,0,0, 0.4)',
+  },
+  rowContainer: {
+    flexDirection: 'row',
   },
 });
